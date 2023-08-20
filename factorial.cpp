7f762120ae8b5367fba1b1b0406f6c0131c6dc18
cs-308-2023 <nehank1026@gmail.com>
2023-08-20 19:16:04 +0530

    Modified

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,8 @@
 #include "functions.h"
 
 int factorial(int n){
-    if (n<0){
-    	return "Re-enter a positive number";
+    if (n<=0){
+    	return "Please re-enter a positive number";
     }
     if(n!=1){
 	return(n * factorial(n-1));
